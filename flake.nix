757dac4fa7458f624b3ed8fd76bbee8e4bda39f1
Ovyerus <ovy@ovyerus.com>
2024-07-01 11:23:56 +1000

    refactor(flake): simplify and drop external dependencies

--- flake.nix
+++ flake.nix
@@ -1,42 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    pnpm2nix = {
-      url = "github:nzbr/pnpm2nix-nzbr";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
-  outputs = {
-    flake-utils,
-    nixpkgs,
-    pnpm2nix,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          overlays = [pnpm2nix.outputs.overlays.default];
-          inherit system;
-        };
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              nodejs
-              nodejs.pkgs.pnpm
-            ];
-          };
-
-        packages.default = pkgs.mkPnpmPackage {
-          src = ./.;
-        };
-
-        formatter = pkgs.alejandra;
-      }
-    );
+  outputs = {nixpkgs, ...}: let
+    forSystems = fn:
+      nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: fn nixpkgs.legacyPackages.${system});
+    defaultForSystems = fn: forSystems (pkgs: {default = fn pkgs;});
+  in {
+    packages = defaultForSystems (pkgs: pkgs.callPackage ./package.nix {});
+    devShells = defaultForSystems (pkgs: with pkgs; mkShell {buildInputs = [nodejs pnpm];});
+  };
 }
---
+++ package.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  stdenv,
+  nodejs,
+  pnpm,
+}: let
+  packageJSON = builtins.fromJSON (builtins.readFile ./package.json);
+in
+  stdenv.mkDerivation (finalAttrs: {
+    pname = packageJSON.name;
+    version = packageJSON.version;
+
+    src = lib.cleanSource ./.;
+
+    pnpmDeps = pnpm.fetchDeps {
+      inherit (finalAttrs) pname version src;
+      hash = "sha256-obhyw372EMvr/jtQzWTYhis1rflxKbg7j9WoBJ+bzpA=";
+    };
+
+    nativeBuildInputs = [nodejs pnpm.configHook];
+
+    buildPhase = ''
+      runHook preBuild
+
+      pnpm build
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -r {dist,package.json,LICENSE,README.md} $out/
+
+      runHook postInstall
+    '';
+  })
